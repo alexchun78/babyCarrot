320e5f624ffdeb79e19a7d8ab29fa5202861eee1
alex <alexchun78@gmail.com>
2024-10-21 21:15:44 +0900

    first commit

---
+++ BabyCarrot/Tools/EmailManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.Configuration;
+
+
+namespace BabyCarrot.Tools
+{
+    public class EmailManager
+    {
+        private MailMessage _MailMassage;
+        private SmtpClient _SmtpClient;
+
+        public EmailManager(string host, int port, string id, string pwd)
+        {
+            _SmtpClient = new SmtpClient(host, port);
+            _SmtpClient.Credentials = new NetworkCredential(id, pwd);
+
+            _MailMassage = new MailMessage();
+            _MailMassage.IsBodyHtml = true;
+            _MailMassage.Priority = MailPriority.Normal;
+        }
+
+        public string From
+        {
+            get { return _MailMassage.From == null ? string.Empty : _MailMassage.From.Address; }
+            set { _MailMassage.From = new MailAddress(value); }
+        }
+
+        public MailAddressCollection To
+        {
+            get { return _MailMassage.To; }
+        }
+
+        public string Subject
+        {
+            get { return _MailMassage.Subject; }
+            set { _MailMassage.Subject = value; }
+        }
+
+        public string Body
+        {
+            get { return _MailMassage.Body; }
+            set { _MailMassage.Body = value; }
+        }
+
+        public void Send()
+        {
+            _SmtpClient.Send(_MailMassage);
+        }
+
+        public static void Send(string from, string to, string subject, string contents, string cc, string bcc)
+        {
+            if (string.IsNullOrEmpty(from))
+            {
+                throw new ArgumentException("sender is empty");
+            }
+            if (string.IsNullOrEmpty(to))
+            {
+                throw new ArgumentException("to is empty");
+            }
+
+
+            string smtpHost = ConfigurationManager.AppSettings["SMTPHost"];
+
+            int smtpPort = 0;
+            if (ConfigurationManager.AppSettings["SMTPPort"] == null ||
+                int.TryParse(ConfigurationManager.AppSettings["SMTPPort"], out smtpPort) == false)
+            {
+                smtpPort = 25;
+            }
+
+            string smtpId = ConfigurationManager.AppSettings["SMTPID"];
+            string smtpPwd = ConfigurationManager.AppSettings["SMTPPassword"];
+
+            MailMessage mailMsg = new MailMessage();
+            mailMsg.From = new MailAddress(from);
+            mailMsg.To.Add(new MailAddress(to));
+
+            if (!string.IsNullOrEmpty(cc))
+                mailMsg.CC.Add(cc);
+
+            if (!string.IsNullOrEmpty(bcc))
+                mailMsg.Bcc.Add(bcc);
+
+            mailMsg.Subject = subject;
+            mailMsg.IsBodyHtml = true;
+            mailMsg.Body = contents;
+            mailMsg.Priority = MailPriority.Normal;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Credentials = new NetworkCredential();
+            smtpClient.Host = smtpHost;
+            smtpClient.Port = smtpPort;
+            smtpClient.Send(mailMsg);
+        }
+
+        public static void Send(string from, string to, string subject, string contents)
+        {
+            Send(from, to, subject, contents, null, null);
+        }
+
+        public static void Send(string to, string subject, string contents)
+        {
+            string sender = ConfigurationManager.AppSettings["SMTPSender"];
+            Send(sender, to, subject, contents);
+        }
+    }
+}
---
+++ BabyCarrotTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using BabyCarrot.Tools;
+using BabyCarrot.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyCarrotTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LogManager log = new LogManager(null, "_test");
+
+            //log.WriteLine("[Begin Processng]...");
+            //for(int index = 0; index < 10;index++)
+            //{
+            //    log.WriteLine("Processing:" + index);
+
+            //    System.Threading.Thread.Sleep(500);
+
+            //    log.WriteLine("Done:" + index);
+            //}
+            //log.WriteLine("[End Processng]...");
+
+            //log.WriteConsole("Extension Test");
+
+            string temp = "1";
+            Console.WriteLine("IsNumeric? : {0}",temp.IsNumeric());
+            Console.WriteLine("IsDateTime? : {0}", temp.IsDateTime());
+            DateTime d = DateTime.Now;
+            Console.WriteLine(d.LastDateOfMonth());
+
+            string content = "Hello, world <br/> this is alex";
+            EmailManager.Send("receiver@gmail.com", "test", content);
+
+            var emailMgr = new EmailManager("", 25, "id", "pwd");
+
+        }
+
+    }
+
+    public static class ExtensionLog
+    {
+        public static void WriteConsole(this LogManager log, string data)
+        {
+            log.Write(data);
+            Console.Write(data);
+        }
+    }
+}
